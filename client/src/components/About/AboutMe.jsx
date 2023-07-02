c1e58bcef2aff54d876e0bc77d05536ec4488ae3
mccoronel <mcelestecrnl@gmail.com>
2023-07-02 19:44:13 -0300

    Cambios DOMINGO 2

--- client/src/components/About/AboutMe.jsx
+++ client/src/components/About/AboutMe.jsx
@@ -4,69 +4,58 @@
 import style from "./AboutMe.module.css";
 import ProfileImg from "../../assets/yo1.jpg";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
-import logoHTML from "../../assets/logoHTML.png";
-import logoJS from "../../assets/logoJS.png";
-import logoREACT from "../../assets/logoREACT.png";
-import logoRedux from "../../assets/logoRedux.png";
-import logoCSS from "../../assets/logoCSS.png";
+
 
 const AboutMe = () => {
   return (
-    <div className={style.content}>
-      
-
-      <div className={style.container}>
-        <div>
-          <img src={logoHTML} alt="html" className={style.logos} />
-          <img src={logoCSS} alt="css" className={style.logoCSS} />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <img src={logoJS} alt="js" className={style.logoJS} />
-          &nbsp;&nbsp;
-          <img src={logoRedux} alt="redux" className={style.logoRedux} />
-          &nbsp;&nbsp;
-          <img src={logoREACT} alt="react" className={style.logos} />
-        </div>
-
-
-        <div className={style.description}>
-          <div className={style.imageContainer}>
-            <img
-              src={ProfileImg}
-              alt="Maria Celeste Coronel"
-              className={style.image}
-            />
-        <div className={style.redes}>
-          <a
-            href="https://www.linkedin.com/in/maria-celeste-coronel-b93837249//"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedin size={70} className={style.linkedin} />
-          </a>
-
-          <a
-            href="https://www.instagram.com/mcelestecrnl/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaInstagram size={70} className={style.instagram} />
-          </a>
-          <a
-            href="https://github.com/MCCoronel/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub size={70} margin-right={50} className={style.github} />
-          </a>
-        </div>
-          </div>
-          <div className={style.texto}>
-          
-          
-          </div>
-        </div>
-      </div>
+    <div className={style.container}>
+    <div className={style.containerName}>
+      <h5 className={style.name}>CORONEL, MARIA CELESTE</h5>
     </div>
+
+    <div className={style.detail}>
+      <div className={style.atributes}>
+        <h2>STATUS | Alive</h2>
+        <h2>&nbsp;&nbsp;&nbsp;GENDER | Female</h2>
+        <h2>&nbsp;&nbsp;&nbsp;SPECIE | Human Developer</h2>
+        <h2>ORIGIN | Earth(C-137)</h2>
+      </div>
+
+     
+        <img
+          className={style.imagen}
+          src={ProfileImg}
+          alt='Maria Celeste Coronel'
+        />
+      
+    </div>
+      <div className={style.redes}>
+              <a
+                href="https://www.linkedin.com/in/maria-celeste-coronel-b93837249//"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin size={70} className={style.linkedin} />
+              </a>
+
+              <a
+                href="https://www.instagram.com/mcelestecrnl/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram size={70} className={style.instagram} />
+              </a>
+              <a
+                href="https://github.com/MCCoronel/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub size={70} margin-right={50} className={style.github} />
+              </a>
+            </div> 
+  </div>
+           
+          
   );
 };
 
