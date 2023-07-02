affed5278c911d03638467b1c8f29473cd1baba4
mccoronel <mcelestecrnl@gmail.com>
2023-07-02 19:11:26 -0300

    Commit Domindo 2

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,27 +1,21 @@
-const { getCharById } = require("../controllers/getCharById.js");
-const { postFav, deleteFav } = require("../controllers/handleFavorites");
-const { login } = require("../controllers/login");
+const getCharById = require("../controllers/getCharById.js");
+const login = require('../controllers/login.js')
+const postFav =  require('../controllers/postFav.js')
+const postUser =  require('../controllers/postUser.js')
+const deleteFav = require('../controllers/deleteFav.js')
+
 
 const router = require("express").Router();
 
-//router.get('/character/:id' , getCharById)
-// todas las pegadas de aqui comienzan con rickandmorty
 router.get("/character/:id", getCharById);
 
 router.get('/login', login)
-// router.get("/login", (req, res) => {
-//   login(req, res);
-// });
+router.post('/login', postUser)
 
 router.post('/fav' , postFav)
-// router.post("/fav", (req, res) => {
-//   postFav(req, res);
-// });
 
 router.delete('/fav/:id' , deleteFav)
 
-// router.delete("/fav/:id", (req, res) => {
-//   deleteFav(req, res);
-// });
+
 
 module.exports = router;
--- server/src/utils/users.js
+++
@@ -1,2 +0,0 @@
-module.exports = [{ email: "credential@gmail.com", password: "credential2" }];
-
